aef2d8e12173eb4b71ad46d1ac66752a6ee16c86
MD-Mustak <mustakarman560@gmail.com>
2025-08-15 16:18:37 +0530

    Data

---
+++ models/listing.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+// Define the listing schema
+const listingSchema = new schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    }
+});
+
+// Create and export the model
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
