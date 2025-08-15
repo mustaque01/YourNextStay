54a7ba71e7118fcd401ad280e80da30a31cb44fd
MD-Mustak <mustakarman560@gmail.com>
2025-08-16 01:52:21 +0530

    commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 
 // Set up view engine and middleware
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
